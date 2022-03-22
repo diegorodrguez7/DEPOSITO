d966bc3ee87b3b6cb5461d65ccf43f69659ba039
1daw26 <1daw26@INFORMATICA-126.DOMDPTOINFJMPP.INF>
2022-03-22 16:32:27 +0100

    Resultado de la operación ha sido Sucsessfull. Updated.

---
+++ src/practicapmd/PracticaPMD.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicapmd;
+
+public class PracticaPMD {
+
+    public static void main(String[] args) {
+        int a;
+        
+        int altura = 89;
+        if (altura > 89) {
+            System.out.println("Eres muy alto");
+        }
+        PracticaPMD ejemPMD = new PracticaPMD();
+        ejemPMD.MetodoDobleReturn();
+        metodo();
+    }
+
+    static void metodo() {
+
+    }
+
+    public boolean MetodoDobleReturn() {
+        int iValor = 10;
+        if (iValor == 5) 
+            return false;
+         else {
+            return true;
+        }
+    }
+
+}
